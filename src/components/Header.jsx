fcad45ba58db9760f553b4f8e7b937f34d6cf104
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-11-29 13:14:53 -0300

    se agrego logo a header, y se corrigio centrado navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import dglogo from '../images/dglogo.png'
+import logo2 from '../images/logo.png'
 import '../css/Header.css'
 
 function Header() {
     return (        
         <header className="header">            
             <a href="/Home">
-                <img className="logo-img" src={dglogo} alt="Diego Gonzalez"/>  
+            <img className="logo-img" src={logo2} alt="Logo veterinaria"/>  
+            <img className="name-img" src={dglogo} alt="Diego Gonzalez"/>                  
             </a>
         </header>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ function Home() {
     return (
         <Layout>
             <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, nam error. Voluptatibus vel veritatis est possimus aliquam explicabo beatae nam inventore aliquid sunt? Quae hic a magni cupiditate amet explicabo?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aspernatur voluptate sint, magni voluptatibus voluptas dolores possimus autem molestiae sed, fuga consequuntur ipsum incidunt cupiditate ratione enim repellendus neque eveniet.</p>            
+            <p>WHATSAPP</p>
         </Layout>        
     )
 }
