6a0f1931626324780d63e64d63bd24a7d4cbd5d3
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-12-17 12:27:42 -0300

    maps added in Contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -19,7 +19,7 @@ function Contact() {
             HORARIO
           </Typography>
           <Typography variant="body1" sx={{ fontSize: '1rem', color: '#333' }}>
-            Horario de lunes a viernes de 09:00 a.m. a 13:00 p.m.
+            Lunes a Viernes de 09:00 a.m. a 13:00 p.m.
           </Typography>
         </Box>
 
@@ -29,16 +29,28 @@ function Contact() {
             UBICACIÓN
           </Typography>
           <Typography variant="body1" sx={{ fontSize: '1rem', color: '#333' }}>
-            Oncativo 320
+            <b>Clínica veterinaria Nefroncovet</b>
           </Typography>
           <Typography variant="body1" sx={{ fontSize: '1rem', color: '#333' }}>
-            Clínica veterinaria Nefroncovet
-          </Typography>
+            Oncativo 320
+          </Typography>          
           <Typography variant="body1" sx={{ fontSize: '1rem', color: '#333' }}>
             Córdoba Capital
           </Typography>
+          <Box sx={{ marginBottom: 4, width: '100%', height: 300 }}>
+            <iframe
+              title="Ubicación de la clínica"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.5726595289366!2d-64.17898492426611!3d-31.41049997409136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94329875fa24017d%3A0xfbf59f9c120a3efd!2sOncativo%20320%2C%20X5000%20C%C3%B3rdoba!5e0!3m2!1sen!2sar!4v1702836537115!5m2!1sen!2sar"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
         </Box>
-
+        <br />
         {/* WhatsApp - Agenda una cita */}
         <Box>          
           <Link 
