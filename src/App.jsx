6bc9069d6d709cb248fe2ee778b1de9e7f521ba0
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-12-18 13:19:44 -0300

    fix error servicio page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Servicios from './pages/Servicios';
-import Servicio from './pages/Servicio';
 import QuienesSomos from './pages/QuienesSomos';
 import Preguntas from './pages/PreguntasFrecuentes';
 import Contacto from './pages/Contacto';
@@ -16,8 +15,7 @@ function App() {
           <Routes>
               <Route index element={<Home />} />
               <Route path="/home" element={<Home />} />            
-              <Route path="/servicios" element={<Servicios />} />                
-              <Route path="/servicios/servicio" element={<Servicio />} />                
+              <Route path="/servicios" element={<Servicios />} />              
               <Route path="/QuienesSomos" element={<QuienesSomos />} />              
               <Route path="/preguntas-frecuentes" element={<Preguntas />} />
               <Route path="/contacto" element={<Contacto />} />
